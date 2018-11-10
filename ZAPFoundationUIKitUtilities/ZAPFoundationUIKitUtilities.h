52c2e8e523f5ffa89e64e7fd5ba4beff6ec5f889
Stefan Pauwels <stefan@pauwels.ch>
2018-11-10 10:45:16 +0100

    Initial Commit

---
+++ ZAPFoundationUIKitUtilities/ZAPFoundationUIKitUtilities.h
@@ -0,0 +1,19 @@
+//
+//  ZAPFoundationUIKitUtilities.h
+//  ZAPFoundationUIKitUtilities
+//
+//  Created by Stefan Pauwels on 10.11.18.
+//  Copyright © 2018 Zozi Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZAPFoundationUIKitUtilities.
+FOUNDATION_EXPORT double ZAPFoundationUIKitUtilitiesVersionNumber;
+
+//! Project version string for ZAPFoundationUIKitUtilities.
+FOUNDATION_EXPORT const unsigned char ZAPFoundationUIKitUtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZAPFoundationUIKitUtilities/PublicHeader.h>
+
+
---
+++ ZAPFoundationUIKitUtilitiesTests/ZAPFoundationUIKitUtilitiesTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZAPFoundationUIKitUtilitiesTests.swift
+//  ZAPFoundationUIKitUtilitiesTests
+//
+//  Created by Stefan Pauwels on 10.11.18.
+//  Copyright © 2018 Zozi Apps. All rights reserved.
+//
+
+import XCTest
+@testable import ZAPFoundationUIKitUtilities
+
+class ZAPFoundationUIKitUtilitiesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
