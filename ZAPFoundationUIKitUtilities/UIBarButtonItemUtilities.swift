29b832833816f03c8319792d158e4622442a0385
Stefan Pauwels <stefan@pauwels.ch>
2018-12-15 23:34:20 +0100

    Empty bar button item

---
+++ ZAPFoundationUIKitUtilities/UIBarButtonItemUtilities.swift
@@ -0,0 +1,15 @@
+//
+//  UIBarButtonItemUtilities.swift
+//  ZAPFoundationUIKitUtilities
+//
+//  Created by Stefan Pauwels on 15.12.18.
+//  Copyright © 2018 Zozi Apps. All rights reserved.
+//
+
+import Foundation
+
+extension UIBarButtonItem {
+    public static func emptyBarButtonItem() -> UIBarButtonItem {
+        return UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
