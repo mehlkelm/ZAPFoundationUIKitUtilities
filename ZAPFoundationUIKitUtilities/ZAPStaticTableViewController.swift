08dcd18dca3fffe3bd699aa5ee102bd7cfad43e4
Stefan Pauwels <stefan@pauwels.ch>
2019-03-28 11:06:44 +0100

    Revert "Static table VC protocolized"
    This reverts commit 0bf8c3783d226227b7afce3a5a9ddccf7fc73f68.

--- ZAPFoundationUIKitUtilities/ZAPStaticTableViewController.swift
+++ ZAPFoundationUIKitUtilities/ZAPStaticTableViewController.swift
@@ -8,7 +8,60 @@
 
 import UIKit
 
-open class ZAPStaticTableViewController: UITableViewController, ZAPStaticTableViewDataSource, ZAPStaticTableViewDelegate {
+open class ZAPStaticTableViewController: UITableViewController {
+    
+    public struct Section {
+        public var headerTitle: String?
+        public var footerTitle: String?
+        public var rows = [Row]()
+        
+        public init(rows: [Row], headerTitle header: String? = nil, footerTitle footer: String? = nil) {
+            self.rows = rows
+            headerTitle = header
+            footerTitle = footer
+        }
+    }
+    
+    public struct Row {
+        public var cell: ((IndexPath) -> (UITableViewCell))
+        public var selectionAction: ((IndexPath) -> (Void))?
+        public var shouldHighlight: Bool = true
+        public var associatedObject: Any? = nil
+        public var height: CGFloat = UITableView.automaticDimension
+        public var estimatedHeight: CGFloat = UITableView.automaticDimension
+        public var indentationLevel: Int = 0
+        public var editable: Bool = false
+        public var commitEdit: ((UITableViewCell.EditingStyle, IndexPath) -> (Void))?
+        public var seguePreparation: ((UIStoryboardSegue, _ sender: Any?, IndexPath) -> Void)?
+        public var ID: String? = nil
+        
+        public init(cell: @escaping ((IndexPath) -> (UITableViewCell)),
+             selectionAction: ((IndexPath) -> (Void))? = nil,
+             shouldHighlight: Bool = true,
+             associatedObject: Any? = nil,
+             height: CGFloat = UITableView.automaticDimension,
+             estimatedHeight: CGFloat = UITableView.automaticDimension,
+             indentationLevel: Int = 0,
+             editable: Bool = false,
+             commitEdit: ((UITableViewCell.EditingStyle, IndexPath) -> (Void))? = nil,
+             seguePreparation: ((UIStoryboardSegue, _ sender: Any?, IndexPath) -> Void)? = nil,
+             ID: String? = nil
+            ) {
+            
+            self.cell = cell
+            self.selectionAction = selectionAction
+            self.shouldHighlight = shouldHighlight
+            self.associatedObject = associatedObject
+            self.height = height
+            self.estimatedHeight = estimatedHeight
+            self.indentationLevel = indentationLevel
+            self.editable = editable
+            self.commitEdit = commitEdit
+            self.seguePreparation = seguePreparation
+            self.ID = ID
+        }
+    }
+    
     public var sections = [Section]()
     
     open override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -17,121 +70,60 @@ open class ZAPStaticTableViewController: UITableViewController, ZAPStaticTableVi
         }
     }
     
+    // MARK: - Table view data source
+    
     public func row(for indexPath: IndexPath) -> Row {
         return sections[indexPath.section].rows[indexPath.row]
     }
-}
-
-protocol ZAPStaticTableViewDataSource: UITableViewDataSource {
-    var sections: [Section] { get set }
-    func row(for indexPath: IndexPath) -> Row
-}
-
-extension ZAPStaticTableViewDataSource {
-    public func numberOfSections(in tableView: UITableView) -> Int {
+    
+    override open func numberOfSections(in tableView: UITableView) -> Int {
         return sections.count
     }
     
-    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    override open func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sections[section].headerTitle
     }
     
-    public func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+    override open func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
         return sections[section].footerTitle
     }
     
-    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override open func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sections[section].rows.count
     }
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return row(for: indexPath).cell(indexPath)
     }
     
-    public func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    override open func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return row(for: indexPath).editable
     }
     
-    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    override open func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         row(for: indexPath).commitEdit?(editingStyle, indexPath)
     }
-}
-
-protocol ZAPStaticTableViewDelegate: UITableViewDelegate {
-    var sections: [Section] { get set }
-    func row(for indexPath: IndexPath) -> Row
-}
-
-extension ZAPStaticTableViewDelegate {
-    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    
+    // MARK: - Table view delegate
+    
+    override open func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return row(for: indexPath).height
     }
     
-    public func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+    override open func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return row(for: indexPath).estimatedHeight
     }
     
-    public func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+    override open func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
         return row(for: indexPath).indentationLevel
     }
     
-    public func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+    override open func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
         return row(for: indexPath).shouldHighlight
     }
     
-    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         row(for: indexPath).selectionAction?(indexPath)
     }
-}
-
-public struct Section {
-    public var headerTitle: String?
-    public var footerTitle: String?
-    public var rows = [Row]()
     
-    public init(rows: [Row], headerTitle header: String? = nil, footerTitle footer: String? = nil) {
-        self.rows = rows
-        headerTitle = header
-        footerTitle = footer
-    }
-}
-
-public struct Row {
-    public var cell: ((IndexPath) -> (UITableViewCell))
-    public var selectionAction: ((IndexPath) -> (Void))?
-    public var shouldHighlight: Bool = true
-    public var associatedObject: Any? = nil
-    public var height: CGFloat = UITableView.automaticDimension
-    public var estimatedHeight: CGFloat = UITableView.automaticDimension
-    public var indentationLevel: Int = 0
-    public var editable: Bool = false
-    public var commitEdit: ((UITableViewCell.EditingStyle, IndexPath) -> (Void))?
-    public var seguePreparation: ((UIStoryboardSegue, _ sender: Any?, IndexPath) -> Void)?
-    public var ID: String? = nil
-    
-    public init(cell: @escaping ((IndexPath) -> (UITableViewCell)),
-                selectionAction: ((IndexPath) -> (Void))? = nil,
-                shouldHighlight: Bool = true,
-                associatedObject: Any? = nil,
-                height: CGFloat = UITableView.automaticDimension,
-                estimatedHeight: CGFloat = UITableView.automaticDimension,
-                indentationLevel: Int = 0,
-                editable: Bool = false,
-                commitEdit: ((UITableViewCell.EditingStyle, IndexPath) -> (Void))? = nil,
-                seguePreparation: ((UIStoryboardSegue, _ sender: Any?, IndexPath) -> Void)? = nil,
-                ID: String? = nil
-        ) {
-        
-        self.cell = cell
-        self.selectionAction = selectionAction
-        self.shouldHighlight = shouldHighlight
-        self.associatedObject = associatedObject
-        self.height = height
-        self.estimatedHeight = estimatedHeight
-        self.indentationLevel = indentationLevel
-        self.editable = editable
-        self.commitEdit = commitEdit
-        self.seguePreparation = seguePreparation
-        self.ID = ID
-    }
 }
