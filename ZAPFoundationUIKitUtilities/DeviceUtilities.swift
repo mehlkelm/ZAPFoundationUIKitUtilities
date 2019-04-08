cd94d7b1d9044ae86f604af30b74b27c0655627e
Stefan Pauwels <stefan@pauwels.ch>
2019-04-08 17:37:15 +0200

    adds UIDevice utilities

---
+++ ZAPFoundationUIKitUtilities/DeviceUtilities.swift
@@ -0,0 +1,27 @@
+//
+//  DeviceUtilities.swift
+//  ZAPFoundationUIKitUtilities
+//
+//  Created by Stefan Pauwels on 05.04.19.
+//  Copyright © 2019 Zozi Apps. All rights reserved.
+//
+
+import Foundation
+
+extension UIDevice {
+    public var isSimulator: Bool {
+        var isSim = false
+        #if arch(i386) || arch(x86_64)
+        isSim = true
+        #endif
+        return isSim
+    }
+    
+    @objc public var isPhone: Bool {
+        return self.userInterfaceIdiom == .phone
+    }
+    
+    public var isTablet: Bool {
+        return self.userInterfaceIdiom == .pad
+    }
+}
