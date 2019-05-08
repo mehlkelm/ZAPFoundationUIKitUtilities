75430e145651efb18290125d9f18afa6940317fe
Stefan Pauwels <stefan@pauwels.ch>
2019-05-08 07:46:18 +0200

    Adds textEncoding to URLResponse

---
+++ ZAPFoundationUIKitUtilities/URLResponseUtilities.swift
@@ -0,0 +1,21 @@
+//
+//  URLResponseUtilities.swift
+//  ZAPFoundationUIKitUtilities
+//
+//  Created by Stefan Pauwels on 02.05.19.
+//  Copyright © 2019 Zozi Apps. All rights reserved.
+//
+
+import Foundation
+
+extension URLResponse {
+    
+    public var textEncoding: String.Encoding {
+        var encoding = String.Encoding.utf8
+        if let encodingName = self.textEncodingName {
+            let CFEncoding = CFStringConvertIANACharSetNameToEncoding(encodingName as CFString)
+            encoding = String.Encoding(rawValue: CFStringConvertEncodingToNSStringEncoding(CFEncoding))
+        }
+        return encoding
+    }
+}
