583c9ff42fac4ebf8662fe83f3a07c1919d8d7db
Stefan Pauwels <stefan@pauwels.ch>
2019-06-09 16:05:03 +0200

    adds css code attribute to UIColor

---
+++ ZAPFoundationUIKitUtilities/UIColorUtilities.swift
@@ -0,0 +1,33 @@
+//
+//  UIColorUtilities.swift
+//  ZAPFoundationUIKitUtilities
+//
+//  Created by Stefan Pauwels on 08.06.19.
+//  Copyright © 2019 Zozi Apps. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+    public var css: String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return NSString(format:"#%06x", rgb) as String
+    }
+    
+    public static func *(left: UIColor, right: CGFloat) -> UIColor {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        
+        left.getRed(&r, green: &g, blue: &b, alpha: &a)
+        let newColor = UIColor(red: r * right, green: g * right, blue: b * right, alpha: a)
+        return newColor
+    }
+}
