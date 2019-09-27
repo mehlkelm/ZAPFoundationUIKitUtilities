b761ed79aa19cd95b65226a2e61b245f5d52d1ae
Stefan Pauwels <stefan@pauwels.ch>
2019-09-27 10:43:23 +0200

    NSMutableAttributedString.setForegroundColor

--- ZAPFoundationUIKitUtilities/NSAttributedStringUtilities.swift
+++ ZAPFoundationUIKitUtilities/NSAttributedStringUtilities.swift
@@ -10,21 +10,31 @@ import Foundation
 
 extension NSAttributedString {
     
-    @objc public convenience init?(HTMLString: String?) {
-        
+    @objc public convenience init?(HTMLString: String?, color: UIColor? = nil) {
         guard let string = HTMLString else {
             return nil
         }
         let data = Data(string.utf8)
-        try? self.init(data: data, options: [ NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html, NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        if let color = color, let mutable = NSMutableAttributedString(HTMLString: HTMLString) {
+            mutable.setForegroundColor(color)
+            self.init(attributedString: mutable)
+        } else {
+            try? self.init(data: data, options: [ NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html, NSAttributedString.DocumentReadingOptionKey.characterEncoding: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        }
     }
     
     @objc public func trailingNewlineChopped() -> NSAttributedString {
-        
         if string.hasSuffix("\n") {
             return self.attributedSubstring(from: NSRange(location: 0, length: length - 1))
         } else {
             return self
         }
+    }
+}
+
+extension NSMutableAttributedString {
+    
+    @objc public func setForegroundColor(_ color: UIColor) {
+        self.addAttribute(.foregroundColor, value: color, range: NSRange(location: 0, length: self.length))
     }
 }
