dbf5f580fae292f652664c6581ba6508c216dbe4
Stefan Pauwels <stefan@pauwels.ch>
2020-01-05 16:52:18 +0100

    Adds getter for split VC masterNavigation VC

---
+++ ZAPFoundationUIKitUtilities/UISplitViewControllerUtilities.swift
@@ -0,0 +1,16 @@
+//
+//  UISplitViewControllerUtilities.swift
+//  ZAPFoundationUIKitUtilities
+//
+//  Created by Stefan Pauwels on 05.01.20.
+//  Copyright © 2020 Zozi Apps. All rights reserved.
+//
+
+import UIKit
+
+extension UISplitViewController {
+    
+    public var masterNavigationController: UINavigationController? {
+        viewControllers.first as? UINavigationController
+    }
+}
